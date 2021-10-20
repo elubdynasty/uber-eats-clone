d0a2f5d1b26886e4473c75117db70406c12ff9fc
Leonard Luzon <leonard_luzon2000@yahoo.com>
2021-10-20 13:50:48 -0700

    Added HeaderTabs comp to have Delivery & Pickup buttons and added TouchableOpacity

---
+++ components/HeaderTabs.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { View, Text, TouchableOpacity } from "react-native";
+
+export default function HeaderTabs() {
+    return (
+      <View>
+        <HeaderButton />
+        <HeaderButton />
+      </View>
+    );
+}
+
+const HeaderButton = () => (
+  <View>
+    <TouchableOpacity>
+        <Text>Delivery</Text>
+    </TouchableOpacity>
+  </View>
+);
