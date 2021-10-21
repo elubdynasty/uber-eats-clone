5591b19e4bf02c8cdc18aca469f3b3b8ded7699b
Leonard Luzon <leonard_luzon2000@yahoo.com>
2021-10-21 10:40:26 -0700

    Added styles to header tabs

--- components/HeaderTabs.js
+++ components/HeaderTabs.js
@@ -1,19 +1,34 @@
 import React from 'react'
-import { View, Text, TouchableOpacity } from "react-native";
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
 
 export default function HeaderTabs() {
     return (
-      <View>
-        <HeaderButton />
-        <HeaderButton />
+      <View style={styles.headercontainer}>
+        <HeaderButton text="Delivery" />
+        <HeaderButton text="Pickup" />
       </View>
     );
 }
 
-const HeaderButton = () => (
-  <View>
-    <TouchableOpacity>
-        <Text>Delivery</Text>
-    </TouchableOpacity>
-  </View>
+const HeaderButton = ({ text }) => (
+  <TouchableOpacity style={styles.headerbackground}>
+    <Text style={styles.headertext}>{text}</Text>
+  </TouchableOpacity>
 );
+
+const styles = StyleSheet.create({
+    
+    headercontainer: {
+        flexDirection: "row",
+        alignSelf: "center"
+    },
+
+    headerbackground: {
+        backgroundColor: "black"
+    },
+
+    headertext: {
+        color: "white"
+    }
+
+})
