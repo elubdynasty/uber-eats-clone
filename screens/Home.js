a3631e750c3c640c8784232ba27b855501f1f04c
Leonard Luzon <leonard_luzon2000@yahoo.com>
2021-10-23 17:15:25 -0700

    Added styles to Home.js and installed react-native-vector-icons and google-places-autocomplete

--- screens/Home.js
+++ screens/Home.js
@@ -1,13 +1,28 @@
 import React from 'react'
-import { View, Text, SafeAreaView } from 'react-native'
+import { View, Text, SafeAreaView, StyleSheet } from 'react-native'
 import HeaderTabs from '../components/HeaderTabs'
 
 export default function Home() {
     return (
-      <SafeAreaView>
-        <HeaderTabs />
+      <SafeAreaView style={styles.safeareacontainer}>
+        <View style={styles.headertabscontainer}>
+          <HeaderTabs />
+        </View>
       </SafeAreaView>
     );
 }
+
+const styles = StyleSheet.create({
+  safeareacontainer: {
+    backgroundColor: "#eee",
+    flex: 1,
+  },
+
+  headertabscontainer: {
+    backgroundColor: "white",
+    padding: 15
+  },
+
+});
 
 //react native f(x) component
