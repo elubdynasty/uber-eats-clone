9f2133fad9a8ab36440d336ae1133c0ca223770e
Leonard Luzon <leonard_luzon2000@yahoo.com>
2021-11-30 10:08:01 -0800

    Added ViewCart button

---
+++ components/restaurantDetail/ViewCart.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import { View, Text, TouchableOpacity } from 'react-native'
+
+const ViewCart = () => {
+    return (
+      <View
+        style={{
+          flex: 1,
+          alignItems: "center",
+          justifyContent: "center",
+          flexDirection: "row",
+          position: "absolute",
+          bottom: 360,
+          zIndex: 999,
+        }}
+      >
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "center",
+            width: "100%",
+          }}
+        >
+          <TouchableOpacity
+            style={{
+              marginTop: 20, //20
+              backgroundColor: "black",
+              alignItems: "center",
+              padding: 13,
+              borderRadius: 30,
+              width: 300,
+              position: "relative",
+            }}
+          >
+            <Text
+              style={{
+                color: "white",
+                fontSize: 20,
+              }}
+            >
+              View Cart
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+}
+
+export default ViewCart
--- screens/RestaurantDetail.js
+++ screens/RestaurantDetail.js
@@ -3,15 +3,17 @@ import { View, Text } from 'react-native'
 import { Divider } from 'react-native-elements'
 import About from '../components/restaurantDetail/About'
 import MenuItems from '../components/restaurantDetail/MenuItems'
+import ViewCart from '../components/restaurantDetail/ViewCart'
 
-const RestaurantDetail = ({ route }) => {
+const RestaurantDetail = ({ route, navigation }) => {
     return (
-        <View>
-            <About route={route}/>
-            <Divider width={1.8} style={{marginVertical: 20}} />
-            <MenuItems />
-        </View>
-    )
+      <View>
+        <About route={route} />
+        <Divider width={1.8} style={{ marginVertical: 20 }} />
+        <MenuItems />
+        <ViewCart navigation={navigation} restaurantName={route.params.name} />
+      </View>
+    );
 }
 
 export default RestaurantDetail
